5cad5fdae2074346a62b24fa2e6cb88ca3d21c0e
shazilhasan <146967418+shazilhasan@users.noreply.github.com>
2023-10-27 21:17:05 +0530

    development.js

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: 'mongodb://localhost/apphera-cockpit-dev',
+	db: 'mongodb://technologics-server:oVT37P2oYORDgYwi8ouLb1wwLm0NSTEzX9qUtSdEqaMMd3OPMx4R2yufCQQRDgimCP0oldWZavahACDbetFXwg==@technologics-server.mongo.cosmos.azure.com:10255/technologics-database?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@technologics-server@',
 	app: {
 		title: 'Apphera - Development Environment'
 	},
