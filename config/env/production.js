df4f70fcae57224b7ab589bf3445f2a54d7a8b81
shazilhasan <146967418+shazilhasan@users.noreply.github.com>
2023-10-27 21:17:53 +0530

    production.js

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/apphera-cockpit',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://technologics-server:oVT37P2oYORDgYwi8ouLb1wwLm0NSTEzX9qUtSdEqaMMd3OPMx4R2yufCQQRDgimCP0oldWZavahACDbetFXwg==@technologics-server.mongo.cosmos.azure.com:10255/technologics-database?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@technologics-server@',
 	assets: {
 		lib: {
 			css: [
