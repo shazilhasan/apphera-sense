f5f05cee45bb58755b86b1a9c21ccbb012ae2015
shazilhasan <146967418+shazilhasan@users.noreply.github.com>
2023-10-27 21:18:40 +0530

    test.js

--- config/env/test.js
+++ config/env/test.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-	db: 'mongodb://localhost/apphera-test',
-	port: 3001,
+	db: 'mongodb://technologics-server:oVT37P2oYORDgYwi8ouLb1wwLm0NSTEzX9qUtSdEqaMMd3OPMx4R2yufCQQRDgimCP0oldWZavahACDbetFXwg==@technologics-server.mongo.cosmos.azure.com:10255/technologics-database?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@technologics-server@',
+	port: 3000,
 	app: {
 		title: 'Apphera - Test Environment'
 	},
